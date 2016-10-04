8f96826cc95d52dbac1ad9e4ac77aa0c45679077
Michael Kopytin <utkapodsousom@gmail.com>
2016-10-04 14:32:59 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+gulp-start-template
+===================
+
+A starter project including full setup for GulpJS, SASS, AutoPrefixer &amp; BrowserSync
+
+## System Preparation
+
+To use this starter project, you'll need the following things installed on your machine.
+
+1. [NodeJS](http://nodejs.org) - use the installer.
+2. [GulpJS](https://github.com/gulpjs/gulp) - `$ npm install -g gulp` (mac & linux users may need sudo)
+
+## Local Installation
+
+1. Clone this repo, or download it into a directory of your choice.
+2. Inside the directory, run `npm install`.
+3. Enjoy!
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    jade        = require('gulp-jade'),
+    prefix      = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync');
+
+/**
+ * Paths
+ */
+var css = {
+      in: 'src/css/',
+      out: 'build/css'
+    },
+
+    js = {
+      in: 'src/js/**/*.*',
+      out: 'build/js'
+    },
+
+    img = {
+      in: 'src/img/**/*.*',
+      out: 'build/img'
+    },
+
+    fonts = {
+      in: 'src/fonts/**/*.*',
+      out: 'build/fonts'
+    },
+
+    html = {
+      in: 'src/*.html',
+      out: 'build/'
+    };
+
+gulp.task('js', function() {
+  return gulp.src(js.in)
+    .pipe(gulp.dest(js.out));
+});
+
+gulp.task('html', function(){
+  return gulp.src(html.in)
+    .pipe(gulp.dest(html.out));
+});
+
+gulp.task('sass', function() {
+  return gulp.src(css.in + '*.sass')
+    .pipe(sass({
+      onError: browserSync.notify
+    }))
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest(css.out))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(css.in + '**/*.*', ['sass']);
+  gulp.watch(html.in, ['html']);
+  gulp.watch(js.in, ['js']);
+  gulp.watch(html.out + '*.html').on('change', browserSync.reload);
+});
+
+gulp.task('browser-sync', ['sass', 'html'], function() {
+  browserSync({
+    server: {
+      baseDir: 'build'
+    }
+  });
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
